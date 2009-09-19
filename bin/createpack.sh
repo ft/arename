20ab45e782e7f625ae5712a0c2435f733f79f5ed
Frank Terbeck <ft@bewatermyfriend.org>
2009-09-19 23:49:37 +0200

    Add createpack.sh and dist.sh from cmc
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ bin/createpack.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+# This is a helper script, that can create tarballs from git repositories.
+# It creates tarballs of the current git HEAD.
+# It can also create different types of tarballs, namely uncompressed,
+# gzip and bzip2 tarballs.
+# usage:
+#   createpack.sh version-string compression-method
+
+POSIX_SHELL=${POSIX_SHELL:-"/bin/sh"}
+
+export LC_ALL=C
+
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1 ; then
+  printf '%s: This is *not* a git repository. Not creating tarballs.\n' "$0"
+  exit 0
+fi
+
+version="$1"
+atype="$2"
+
+if [ -z "${version}" ] || [ -z "${atype}" ] ; then
+    printf 'usage: %s version-string compression-method\n' "$0"
+    exit 1
+fi
+
+package="$( pwd )"
+package="${package##*/}"
+package="${package%.git}"
+
+COMMON="${package}-${version}"
+COMMON="$( echo ${COMMON} | sed 's,-v,-,' )"
+TARNAME="${COMMON}.tar"
+PREFIX="${COMMON}/"
+export package version COMMON TARNAME PREFIX
+
+git archive --format=tar --prefix="${PREFIX}" HEAD > "${TARNAME}"
+if [ -e ./buildsys/createpack.hook.sh ] ; then
+    # Adding files, that are needed in tarballs.
+    ${POSIX_SHELL} ./buildsys/createpack.hook.sh
+fi
+
+case "${atype}" in
+    bz2)
+        printf '  - creating bzip2 tarball: %s\n' "${TARNAME}.bz2"
+        bzip2 -f -9 "${TARNAME}"
+        ;;
+    gz)
+        printf '  - creating gzip tarball: %s\n' "${TARNAME}.gz"
+        gzip -f -9 "${TARNAME}"
+        ;;
+    tar)
+        printf '  - creating uncompressed tarball: %s\n' "${TARNAME}"
+        ;;
+    *)
+        printf 'unknown value in \$atype. This should not happen.\n'
+        ;;
+esac
---
+++ bin/dist.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+
+# Automatically create a tarballs.
+#
+# This can also create a tarball for the *latest* release and prerelease
+# tag, if available.
+#
+# usage:
+#   dist.sh [-n|-p] [version-identifier]
+#
+#       -n      create a new release; take the version-identifier argument
+#               as the name for the new release's tag.
+#       -r      when called without version-identifier argument, create
+#               a tarball for the latest release.
+#       -p      when called without version-identifier argument, create
+#               a tarball for the latest prerelease.
+#       -s      when called without version-identifier argument, create
+#               a tarball for the current git HEAD.
+#
+# examples:
+#   Create a new tag and tarball for release 0.4:
+#       % dist.sh -n v0.4
+#   Create a tarball of the latest release tag in history:
+#       % dist.sh
+#   Create a tarball of the latest prerelease tag in history:
+#       % dist.sh -p
+#   TODO: This is currently missing:
+#   Create a tarball of the existing v0.3 release:
+#       % dist.sh v0.3
+
+POSIX_SHELL=${POSIX_SHELL:-"/bin/sh"}
+
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1 ; then
+  printf '%s: This is *not* a git repository. Not creating tarballs.\n' "$0"
+  exit 0
+fi
+
+prerelease=0
+newrelease=0
+release=0
+snapshot=0
+
+while [ -n "$1" ] ; do
+    case "$1" in
+    -n)
+        newrelease=1
+        shift
+        ;;
+    -r)
+        release=1
+        shift
+        ;;
+    -p)
+        prerelease=1
+        shift
+        ;;
+    -s)
+        snapshot=1
+        shift
+        ;;
+    -*)
+        printf 'Unknown option %s.\n' "$1"
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *)
+        break
+        ;;
+    esac
+done
+
+if [ -z "$1" ] ; then
+    if [ "${release}" -gt 0 ] ; then
+        rel_version="v$(${POSIX_SHELL} ./buildsys/bin/getversion.sh release)"
+    fi
+    if [ "${prerelease}" -gt 0 ] ; then
+        pre_version="v$(${POSIX_SHELL} ./buildsys/bin/getversion.sh prerelease)"
+    fi
+    if [ "${snapshot}" -gt 0 ] ; then
+        snap_version="v$(${POSIX_SHELL} ./buildsys/bin/getversion.sh snapshot)"
+        case "${snap_version}" in
+        *-dirty)
+            printf 'Building tarballs of -dirty versions does not work! Abort.\n'
+            exit 1
+            ;;
+        esac
+    fi
+    newrelease=0
+else
+    version="$1"
+fi
+
+if [ "${newrelease}" -gt 0 ] ; then
+    if [ -n "${release}" ] || [ -n "${prerelease}" ] || [ -n "${snapshot}" ]; then
+        printf 'Sorry, no other options allowed beside -n. Abort.\n'
+        exit 1
+    fi
+    [ -z "${version}" ] && exit 0
+
+    printf 'New release requested. Tagging current master head as '\''%s'\'.'\n' "${version}"
+    case "${version}" in
+        v*.*) ;;
+        *) printf ' -!- New version tag looks suspiciously wrong; Are you sure?\n' ;;
+    esac
+    printf ' Proceed? (yes/no) '
+    read answer
+    if [ "${answer}" != 'yes' ] ; then
+        printf 'Did not get a '\''yes'\'', aborting.\n'
+        exit 1
+    fi
+    git tag -s -m "Release ${version}" "${version}"
+fi
+
+if [ "${release}" -gt 0 ] && [ -n "${rel_version}" ] && [ "${rel_version}" != 'v' ]; then
+    git checkout "${rel_version}"
+    ${POSIX_SHELL} ./buildsys/bin/createpack.sh "${rel_version}" gz
+fi
+if [ "${prerelease}" -gt 0 ] && [ -n "${pre_version}" ] && [ "${pre_version}" != 'v' ]; then
+    git checkout "${pre_version}"
+    ${POSIX_SHELL} ./buildsys/bin/createpack.sh "${pre_version}" gz
+fi
+if [ "${snapshot}" -gt 0 ] && [ -n "${snap_version}" ] && [ "${snap_version}" != 'v' ]; then
+    ${POSIX_SHELL} ./buildsys/bin/createpack.sh "${snap_version}" gz
+fi
+if [ "${newrelease}" -gt 0 ] && [ -n "${version}" ] && [ "${version}" != 'v' ]; then
+    ${POSIX_SHELL} ./buildsys/bin/createpack.sh "${version}" gz
+fi
+
+exit 0
