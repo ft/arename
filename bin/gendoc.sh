a91edea8d34b706519bf9378132575f425e1bb2c
Frank Terbeck <ft@bewatermyfriend.org>
2015-05-24 12:11:36 +0200

    Do not suppress errors from pod2man anymore
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- bin/gendoc.sh
+++ bin/gendoc.sh
@@ -8,13 +8,13 @@ fi
 
 if [ "${NAME}" -nt "${NAME}.1" ] || [ ! -e "${NAME}.1" ] ; then
     printf 'POD2MAN  %s > %s\n'  "${NAME}"   "${NAME}.1"
-    pod2man  --name="${NAME}"  ./"${NAME}" > "${NAME}.1"    2>/dev/null \
+    pod2man  --name="${NAME}"  ./"${NAME}" > "${NAME}.1" \
         || exit 1
 fi
 
 if [ ${NAME} -nt ${NAME}.html ] || [ ! -e "${NAME}.html" ]; then
     printf 'POD2HTML %s > %s\n'  "${NAME}"   "${NAME}.html"
-    pod2html                   ./"${NAME}" > "${NAME}.html" 2>/dev/null \
+    pod2html                   ./"${NAME}" > "${NAME}.html" \
         || exit 1
     rm -f *.tmp
 fi
